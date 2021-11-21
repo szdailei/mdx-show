1577b8948dc64d1813400b11908c4917d6e7c465
Dailei <szdailei@gmail.com>
2021-11-21 15:11:48 +0800

    feat: update to dev-sciprts 0.6.0

--- scripts/exe/build-exe.js
+++ scripts/exe/build-exe.js
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { exec } from 'pkg';
-import { mjsToCjs } from '@szdailei/dev-scripts/scripts/index.js';
+import { buildCliServer, mjsToCjs } from '@szdailei/dev-scripts/scripts/index.js';
 
 async function cjsToExe(cjsFile, exeFile) {
   await exec([cjsFile, '--target', 'linux-x64,win-x64', '--output', exeFile]);
 }
 
 async function buildExe() {
+  await buildCliServer()
+
   const mjs = 'cli-server.js';
   const cjs = 'cli-server.cjs';
   const { cjsFile, exeFile } = await mjsToCjs(mjs, cjs);
